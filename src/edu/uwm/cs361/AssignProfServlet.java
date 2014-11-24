e719b91dcda662c81376b3d35a7fc823aabf7320
njmaerz <nathanmaerz@gmail.com>
2014-11-23 18:29:39 -0600

    AssignProf
    AssignProf is now working. I just need to refactor the code some

--- src/edu/uwm/cs361/AssignProfServlet.java
+++ src/edu/uwm/cs361/AssignProfServlet.java
@@ -2,12 +2,13 @@ package edu.uwm.cs361;
 
 import java.io.IOException;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.google.appengine.api.datastore.*;
 
 @SuppressWarnings("serial")
 public class AssignProfServlet extends HttpServlet implements CallBack
@@ -17,6 +18,10 @@ public class AssignProfServlet extends HttpServlet implements CallBack
 	private HttpServletRequest _req;
 	
 	private String _courseID;
+	
+	private String _sectionID;
+	
+	private String _holdID="";
 	
 	private HttpServletResponse _resp;
 	
@@ -28,8 +33,7 @@ public class AssignProfServlet extends HttpServlet implements CallBack
 	
 	private List<Section> _sections;
 	
-	private List<User> _users;
-	
+	private int index=0;
 	
 	@Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
@@ -40,15 +44,21 @@ public class AssignProfServlet extends HttpServlet implements CallBack
 		
 		_courses = Datastore.getCourses(null);
 		
+		_errors = new ArrayList<String>();
+		
 		_courseID = getCourseID();
+		
+		_sectionID = "";
 		
 		_sections = Datastore.getSections(null);
 		
-		_users = Datastore.getUsers(null);
-		
 		_form = new Form(_req, _resp, new ArrayList<String>());
 		
-		_form.handleGet("Assign Proffesor", 3, this, "", ACCESS_LEVEL);
+		save();
+		
+		_form.handleGet("Assign Proffesor", 1, this, "", ACCESS_LEVEL);
+		
+		
 	}
 	
 	@Override
@@ -70,7 +80,7 @@ public class AssignProfServlet extends HttpServlet implements CallBack
 			_resp.getWriter().println(
 		"</table>"+
 		"<div class='submit'>"+ 
-			"<input type='button' class='button' value='Save' />"+
+			"<input type='submit' name='submit' class='button' value='Save' />"+
 		"</div>"+
 	"</form>"+
 	"</div>");
@@ -78,10 +88,16 @@ public class AssignProfServlet extends HttpServlet implements CallBack
 	
 	@Override
 	public void validate(){	
+		for(int i=0;i<=index;++i){
+			if(!isValid(_req.getParameter("profName"+i))){
+				_errors.add("Name "+(i+1)+" is invalid");
+				System.out.println("Name "+(i+1)+" is invalid");
+			}
+		}
 	}
 	
 	private void printCourse() throws IOException{
-		String html = "<form action='assign-prof' method='post' class='standard-form'>"
+		String html = "<form action='course' method='post' class='standard-form'>"
 				+"<select name='course'>";
 		
 		Datastore ds = new Datastore(_req, _resp, _errors);
@@ -89,20 +105,18 @@ public class AssignProfServlet extends HttpServlet implements CallBack
 		{
 			
 		for(Course  course : _courses) {
-			
-
-			if(course.getID().equals(_courseID))
+			if(course.getID().equals(_courseID)){
 				html += "<option selected='selected' value='"+course.getID()+"'>"+course.getName()+"</option>";
-			
-			else
+				_sectionID=course.getID();
+			}
+			else{
 				html += "<option value='"+course.getID()+"'>"+course.getName()+"</option>";
-
+			}
 		}
-			
-		
 		}
 		else
 		{
+			boolean firstTime= true;
 			boolean containsProf = false;
 			for(Course  course : _courses) {
 				for(Section section: _sections){
@@ -111,42 +125,103 @@ public class AssignProfServlet extends HttpServlet implements CallBack
 						containsProf = true;
 					}		
 				}
-			if(course.getID().equals(_courseID)&& containsProf )
+			if(course.getID().equals(_courseID)&& containsProf ){
 				html += "<option selected='selected' value='"+course.getID()+"'>"+course.getName()+"</option>";	
-			
-			else if(containsProf)
+				firstTime=false;
+				_sectionID=course.getID();
+			}
+			else if(containsProf){
 				html += "<option value='"+course.getID()+"'>"+course.getName()+"</option>";
-			
+				if(firstTime)
+				{
+					_sectionID=course.getID();
+					firstTime=false;
+				}
+			}
 			containsProf=false;
 			}
 		}
 		html += "</select>"
-				+"<div class='submit'><input type='submit' name='submit' class='button' value='Submit' /></div>"
+				+"<div class='submit'><input type='submit' name='save' class='button' value='Submit' /></div>"
 			+"</form>";
 		_resp.getWriter().println(html);
 	}
 	
 	private void printSection() throws IOException{
-		String html ="<table class='assign-table'>"+
+		Datastore ds = new Datastore(_req, _resp, _errors);
+		String html ="<form action='assign-prof' method='post' class='standard-form'>"+
+				"<table class='assign-table'>"+
 			"<tr>"+
 				"<td><strong> Class Type</strong></td>"+
 				"<td><strong> Section #</strong></td>"+
 				"<td><strong> Current Instructor</strong></td>"+         
 			"</tr>";
+		index =0;
 		
+		String name="profName";
 		 for(Section section: _sections){
-			 if(section.getCourseID().equals(_courseID)){
+			 name="profName"+index;
+			 if(section.getCourseID().equals(_sectionID) && Integer.parseInt(ds.getUser().getAccess())==ACCESS_ADMIN){
 				 html += "<tr>"+
 							"<td><span>"+section.getClassType() +"</span></td>"+
 							"<td><span>"+section.getSection()+"</span></td>"+
-							"<td><input value='"+section.getInstructor()+"' type='textbox'></td>"+         
+							"<td><input value='"+section.getInstructor()+"' type='textbox' name='"+name+"'></td>"+         
 						"</tr>";
+				
+				index +=1;
 			 }
+			 
+			 else if(section.getCourseID().equals(_sectionID)){
+				 if(section.getClassType().equals("LEC")){
+					 html += "<tr>"+
+							"<td><span>"+section.getClassType() +"</span></td>"+
+							"<td><span>"+section.getSection()+"</span></td>"+
+							"<td><input value='"+section.getInstructor()+"' type='textbox' name='"+name+"' readonly='readonly'></td>"+         
+						"</tr>";
+					 index+=1;
+				 }
+				 else{
+					 html += "<tr>"+
+								"<td><span>"+section.getClassType() +"</span></td>"+
+								"<td><span>"+section.getSection()+"</span></td>"+
+								"<td><input value='"+section.getInstructor()+"' type='textbox' name='"+name+"'></td>"+         
+							"</tr>";
+					index+=1;
+				 }
+			 }
+			 
 		 }
-		 
+		 html+="</form>";
 		 _resp.getWriter().println(html);
+		 System.out.println(_sectionID);
+		 _holdID=_sectionID;
 	}
-	private String getCourseID() {
+	
+	private void save(){
+		if (_req.getParameter("submit")!= null){
+			validate();
+			if(_errors.size() == 0){
+				Datastore ds = new Datastore(_req, _resp, _errors);
+				System.out.println(_holdID);
+				ds.updateProf(_holdID);
+			}
+		}
+	}
+	
+	
+	private boolean isValid(String input){
+		if(input!=null && !input.isEmpty()){
+			Pattern pattern = Pattern.compile("[a-zA-Z]+[,]+[ ]+[a-zA-z]+([ '-][a-zA-Z]+)*");
+			Matcher matcher = pattern.matcher(input);
+			if(matcher.matches())
+				return true;
+			else
+				return false;
+		}
+		return true;
+	}
+	
+	public String getCourseID() {
 
 		return (_req.getParameter("course") != null ? 
 				_req.getParameter("course") : _courses.get(0).getID());
