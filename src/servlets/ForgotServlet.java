ff4bb05d2111552adda1afbd5758ba7a7c28da26
dee-lewis <dwlewis@uwm.edu>
2014-12-10 14:45:22 -0600

    Added email functionality for Forgot Password (Needs testing)

--- src/servlets/ForgotServlet.java
+++ src/servlets/ForgotServlet.java
@@ -1,7 +1,9 @@
 package servlets;
 
 import java.io.IOException;
-import java.util.ArrayList;
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+import java.util.Properties;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -9,30 +11,88 @@ import javax.servlet.http.HttpServletResponse;
 
 import edu.uwm.cs361.Datastore;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import jdo.User;
+
 /**
  * Class for forgotten credentials
  * @author 5guys
  */
 @SuppressWarnings("serial")
 public class ForgotServlet extends HttpServlet
-{
+{	
 	@Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
     
-		
 		Datastore.addAdmin();
 		
-		//Datastore ds = new Datastore(req, resp, new ArrayList<String>(0));
-		
-		resp.getWriter().println();
-		resp.getWriter().println("Created One Admin");
-		
-		resp.sendRedirect("forgot.html");
+		sendEmail(req, resp);
 	}
 	
+
 	@Override
 	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		
-		doGet(req, resp);
+		printContent(resp);
+	}
+	
+	private void printContent(HttpServletResponse resp) throws IOException {
+		
+		resp.setContentType("text/html");
+		
+		resp.getWriter().println("Enter Username: <input type='text' name='email' value='' />"
+				 +"<input type='button' name='submit' value='submit' />");
+		
+	}
+	
+	private void sendEmail(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		
+		List<User> users = Datastore.getUsers("Email=='"+req.getParameter("Email")+"'");
+		
+		if(users.size() > 0) {
+			
+			User user = users.get(0);
+			
+			Properties props = new Properties();
+			
+			Session session = Session.getDefaultInstance(props, null);
+	
+			String msgBody = "Password: "+user.getPassword();
+	
+			try {
+	
+				InternetAddress from = new InternetAddress("fiveguys.uwm@gmail.com", "Password Recovery");
+				
+				InternetAddress to = new InternetAddress(user.getEmail(), user.getFirstName()+" "+user.getLastName());
+				
+				Message msg = new MimeMessage(session);
+				
+				msg.setFrom(from);
+				
+				msg.addRecipient(Message.RecipientType.TO, to);
+				
+				msg.setSubject("Password Recovery");
+				
+				msg.setText(msgBody);
+				
+				Transport.send(msg);
+	
+			} catch (MessagingException | UnsupportedEncodingException e) {
+				
+				e.printStackTrace();
+			}
+			
+			resp.getWriter().println("Email Sent");
+			
+		} else {
+			
+			resp.getWriter().println("Email Not Found");
+		}
 	}
 }
